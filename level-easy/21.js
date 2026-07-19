1128b844c1465a514eca8d36fc8eb8a00a00e361
Bildan Jauhary <bildanjauhary@gmail.com>
2026-07-19 17:08:05 +0700

    feat: add more leetcode task result

---
+++ level-easy/21.js
@@ -0,0 +1,47 @@
+/*
+1512. Number of Good Pairs
+
+Given an array of integers nums, return the number of good pairs.
+
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+Example 2:
+
+Input: nums = [1,1,1,1]
+Output: 6
+Explanation: Each pair in the array are good.
+Example 3:
+
+Input: nums = [1,2,3]
+Output: 0
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function(nums) {
+    const newM = new Map()
+    let count = 0
+
+    for(num of nums){
+        if(newM.has(num)){
+            count += (newM.get(num))
+            newM.set(num, (newM.get(num)+1))
+        } else newM.set(num, 1)
+    }
+    return count
+};
